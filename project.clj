0d875614d19dcce30ebdde7bb8b62c7a5b7dbc10
Eugene Sinitsyn <djorka91@mail.ru>
2017-11-29 08:05:59 +0300

    fix project clj

--- project.clj
+++ project.clj
@@ -4,7 +4,9 @@
   :min-lein-version "2.0.0"
   :dependencies [[org.clojure/clojure "1.8.0"]
                  [compojure "1.5.1"]
-                 [ring/ring-defaults "0.2.1"]]
+                 [ring/ring-defaults "0.2.1"]
+                 [org.clojure/java.jdbc "0.6.1"]
+                 [org.postgresql/postgresql "9.4-1201-jdbc41"]]
   :plugins [[lein-ring "0.9.7"]]
   :ring {:handler hs-test-problem.handler/app}
   :profiles
