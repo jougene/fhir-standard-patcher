ecbd48ec315123e384789498c0397f3e606256b7
Eugene Sinitsyn <djorka91@mail.ru>
2017-11-29 08:56:20 +0300

    add connection to local psql database

---
+++ src/hs_test_problem/db.clj
@@ -0,0 +1,14 @@
+(ns hs-test-problem.db
+  (:require [clojure.java.jdbc :as j]))
+
+(def db
+  {:classname "org.postgresql.Driver"
+   :subprotocol "postgresql"
+   :subname "//localhost:5432/fhir_standart_patcher"
+   :user "fhir"
+   :password "12345"
+   :sslmode "require"
+   })
+
+(j/query db
+   ["select now()"])
