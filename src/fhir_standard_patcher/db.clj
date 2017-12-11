feb6d1f74286a3cb2626b6c8cda75e04d4481b10
Your Name <you@example.com>
2017-12-11 17:43:43 +0300

    add db conn, working with honey sql

--- src/fhir_standard_patcher/db.clj
+++ src/fhir_standard_patcher/db.clj
@@ -1,14 +1,35 @@
 (ns fhir-standard-patcher.db
-  (:require [clojure.java.jdbc :as j]))
+  (:require 
+    ; [java-jdbc.ddl :as ddl]
+    [clojure.java.jdbc :as j]
+    [honeysql.core :as sql]
+    [honeysql.helpers :refer :all]
+    [honeysql-postgres.format :refer :all]
+    [honeysql-postgres.helpers :refer :all]
+    ;; 
+    ))
 
-(def db
+(def db-conn
   {:classname "org.postgresql.Driver"
    :subprotocol "postgresql"
-   :subname "//localhost:5432/fhir_standart_patcher"
+   :subname "//localhost:5432/fhir?sslmode=disable"
    :user "fhir"
    :password "12345"
    :sslmode "require"
    })
 
-(j/query db
-   ["select now()"])
+(def create-resources-sql
+  (-> (create-table :resources)
+      (with-columns 
+         [[:id :integer (sql/call :primary-key)]
+          [:type (sql/call :varchar 32) (sql/call :not nil)]
+          [:data :text]])
+       sql/format))
+
+(j/query db-conn create-resources-sql)
+(j/query db-conn (-> (select :*)
+                     (from :resources)
+                     sql/format)
+(j/query db-conn
+         (sql/format (drop-table :resources)))
+         
