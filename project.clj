ed36361fc3796348e27600532bdb2622661c2fc5
Eugene Sinitsyn <djorka91@mail.ru>
2017-12-13 09:13:12 +0300

    add migratus library, add migrations examples

--- project.clj
+++ project.clj
@@ -3,19 +3,30 @@
   :url "http://example.com/FIXME"
   :min-lein-version "2.0.0"
   :dependencies [[org.clojure/clojure "1.8.0"]
+                 [environ "0.5.0"]
                  [ring "1.6.3"]
+                 [ring/ring-json "0.4.0"]
                  [compojure "1.5.1"]
                  [rum "0.10.8" :only [server-render]]
+                 ;; db deps
                  [org.clojure/java.jdbc "0.6.1"]
                  [org.postgresql/postgresql "9.4-1201-jdbc41"]
-                 [java-jdbc/dsl "0.1.3"]
                  [nilenso/honeysql-postgres "0.2.3"]
-                 [ring/ring-json "0.4.0"]
+                 [migratus "1.0.1"]
+                 ;; frontend
                  [org.clojure/data.json "0.2.6"]
                  [org.clojure/clojurescript "1.9.946"]]
                  ; [cheshire "5.8.0"]
-  :plugins [[lein-ring "0.9.7"]]
+  :plugins [[lein-ring "0.9.7"]
+            [migratus-lein "0.5.3"]]
   :ring {:handler fhir-standard-patcher.handler/app}
-  :profiles
-  {:dev {:dependencies [[javax.servlet/servlet-api "2.5"]
-                        [ring/ring-mock "0.3.0"]]}})
+  :profiles {:dev {:dependencies [[javax.servlet/servlet-api "2.5"]
+                        [ring/ring-mock "0.3.0"]]}}
+  :migratus {:store :database
+             :migration-dir "migrations"
+             :db {:classname   "org.postgresql.Driver"
+                  :subprotocol "postgresql"
+                  :subname     "//localhost:5432/fhir?sslmode=disable"
+                  :user        "fhir"
+                  :password    "12345"
+                  :sslmode     "require"}})
---
+++ resources/migrations/20171213084603-create-resource-table.up.sql
@@ -0,0 +1,5 @@
+CREATE TABLE resources (
+    id      integer PRIMARY KEY,
+    type    varchar(32) NOT NULL,
+    data    text
+);
